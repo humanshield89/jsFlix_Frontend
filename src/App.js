ebf0ffa78ef7e29a37d5d1feb40724f1246b19c1
humanshield89 <rachidboudjelida@gmail.com>
2020-04-17 02:32:30 +0100

    add Appbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
 import YifyContainer from "./Components/YifyContainer";
-import {IOptions as classes} from "glob";
-
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import {makeStyles} from "@material-ui/core/styles";
+import {fade} from "@material-ui/core";
+import MyAppBar from "./Components/MyAppBar";
 
 function App() {
 
   return (
     <div className="App">
-        <header className="pageHeader">
-          <h1>JSFlix</h1>
-        </header>
+      <MyAppBar />
       <YifyContainer />
         </div>
   );
---
+++ src/Components/MyAppBar.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import {fade, makeStyles} from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(1),
+            width: 'auto',
+        },
+    },
+}));
+
+export default function SearchAppBar() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <AppBar /**position="static"*/ color="primaryLight">
+                <Toolbar>
+                    <IconButton
+                        edge="start"
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="open drawer"
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography className={classes.title} variant="h6" noWrap>
+                        JSFlix
+                    </Typography>
+                    <IconButton
+                        edge="end"
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="profile">
+
+                        <AccountCircleIcon/>
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
